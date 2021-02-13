3d2c028987d4c585b1f7deed95f998ac73ace887
Ian Baker <inbox@ianbaker.me>
2021-02-13 17:12:30 +0000

    Added ng-bootstrap

--- WebApi.Host/ClientApp/src/app/home/home.component.ts
+++ WebApi.Host/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { NgbCalendar, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+  model: NgbDateStruct;
+  date: { year: number, month: number };
+
+  constructor(private calendar: NgbCalendar) { }
 }
